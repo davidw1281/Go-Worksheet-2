9d4da5f26601368fcd084b597ff6038d44313538
David <davidwusu128@icloud.com>
2024-09-19 18:39:35 +0100

    Ping

--- ping/ping.go
+++ ping/ping.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"runtime/trace"
@@ -8,18 +9,29 @@ import (
 )
 
 func foo(channel chan string) {
-	// TODO: Write an infinite loop of sending "pings" and receiving "pongs"
+	for {
+		fmt.Println("Foo is sending: ping")
+		channel <- "ping"
 
+		<-channel
+		fmt.Println("Foo has received: Pong")
+	}
 }
 
 func bar(channel chan string) {
-	// TODO: Write an infinite loop of receiving "pings" and sending "pongs"
+	for {
+		<-channel
+		fmt.Println("Bar has received: ping")
+
+		fmt.Println("Bar is sending: pong")
+		channel <- "pong"
+	}
 }
 
 func pingPong() {
-	// TODO: make channel of type string and pass it to foo and bar
-	go foo(nil) // Nil is similar to null. Sending or receiving from a nil chan blocks forever.
-	go bar(nil)
+	channel := make(chan string)
+	go foo(channel) // Nil is similar to null. Sending or receiving from a nil chan blocks forever.
+	go bar(channel)
 	time.Sleep(500 * time.Millisecond)
 }
 
